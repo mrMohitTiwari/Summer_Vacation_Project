29d06811c65ead37427242eb76e75748099855c1
Mohit Tiwari <mt0988145@gmail.com>
2026-05-09 12:52:16 +0530

    all types of insertion complete

--- DSA_Revisit/LINKEDLIST/implementation.cpp
+++ DSA_Revisit/LINKEDLIST/implementation.cpp
@@ -90,19 +90,18 @@ Node *delteK(Node *head, int k)
     // now we will search for kth element
     int cnt = 1;
     Node *temp = head;
-    Node *prev=nullptr;
+    Node *prev = nullptr;
     while (temp)
     {
-         cnt++;
-         prev = temp;
-         temp = temp->next;
+        cnt++;
+        prev = temp;
+        temp = temp->next;
         if (cnt == k)
         {
             prev->next = prev->next->next;
             delete (temp);
             break;
         }
-       
     }
     return head;
 }
@@ -120,24 +119,114 @@ Node *deltedata(Node *head, int val)
         return head;
     }
     // now we will search for kth element
- 
+
     Node *temp = head;
-    Node *prev=nullptr;
+    Node *prev = nullptr;
     while (temp)
     {
-      
-         prev = temp;
-         temp = temp->next;
-        if (temp->data==val)
+
+        prev = temp;
+        temp = temp->next;
+        if (temp->data == val)
         {
             prev->next = prev->next->next;
             delete (temp);
             break;
         }
-       
     }
     return head;
 }
+
+// ************** Insertion ****************************************
+Node *InsetAtHead(Node *head, int val)
+{
+    if (!head)
+    {
+        // head->data = val;
+        // head->next = NULL;
+        // return head;
+        return new Node(val);
+    }
+    // create a node
+    Node *temp = new Node(val);
+    temp->next = head;
+    head = temp;
+    return head;
+}
+// inserting at k position
+Node *insertK(Node *head, int k, int val)
+{
+    if ((!head) && k == 1)
+        return new Node(val);
+    if (k == 1)
+    {
+        Node *temp = new Node(val);
+        temp->next = head;
+        head = temp;
+        return head;
+    }
+    //    inseting at some other k place
+    int cnt = 0;
+    Node *temp = head;
+    while (temp)
+    {
+        cnt++;
+        if (cnt == k - 1)
+        {
+            // create a node
+            Node *newNode = new Node(val);
+            newNode->next = temp->next;
+            temp->next = newNode;
+            break;
+        }
+        temp = temp->next;
+    }
+
+    return head;
+}
+// insert at head
+Node* insetAtTail(Node*head ,int val)
+{
+    if(!head) return new Node(val);
+        Node *temp = head;
+        while(temp) temp= temp->next;
+            // create new node and add it 
+temp->next = new Node(val);
+return head;
+ 
+
+}
+// inseting before a given value
+Node *insertingBeforVal(Node *head, int k, int val)
+{
+   if(!head) return NULL; //if head is null we do not have any value before which we can insert
+    if (head->data == val)
+    {
+        Node *temp = new Node(val);
+        temp->next = head;
+        head = temp;
+        return head;
+    }
+    //    inseting at some other k place
+
+    Node *temp = head;
+    while (temp->next)
+    {
+        
+if(temp->next->data == val)
+        {
+            // create a node
+            Node *newNode = new Node(val);
+            newNode->next = temp->next;
+            temp->next = newNode;
+            break;
+        }
+        temp = temp->next;
+    }
+
+    return head;
+}
+
 int main()
 {
     // static variable
@@ -155,8 +244,10 @@ int main()
     // deleteLast(head);
     // deleting the kth node
     // head = delteK(head, 9);
-    head = deltedata(head,3);
-
+    // head = deltedata(head,3);
+    // cout<<"inserting at head\n";
+    // head= InsetAtHead(head,40);
+        head =insertK(head,10,100);
     PrintLL(head);
 
     return 0;
