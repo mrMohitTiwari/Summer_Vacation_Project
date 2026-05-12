fe22c75aaf24c8c1f981110514e346f7bd212426
Mohit Tiwari <mt0988145@gmail.com>
2026-05-12 12:37:49 +0530

    starting with python

---
+++ 12_May_python/Bascis.py
@@ -0,0 +1,16 @@
+# how to check type of dataype
+name = "mohit"
+x=12
+h = True
+arr = [1,2,3]
+print(name,x,h,arr)
+# fstring 
+print(f"This is a fstring {name}")
+print("checking the type of data")
+print(type(name))
+print(type(arr))
+# swapping two number
+a = 2
+b=3
+a , b = b,a
+print(f"new value of a and b are {a} and {b}")
---
+++ 12_May_python/repl.py
@@ -0,0 +1,24 @@
+# in this we will be learning about repl -> read evalute print loop
+# it will be in terminal
+# printing the same name 5 times : "mohit "*5
+# for finding the lenght of the string
+# len("jansjdaj djan")
+# reading from the keyboard 
+# calcualtor in repl
+# num1 = int(input("Enter the first number"))
+# num2 = int((input("Enter the second number")))
+# print("Addition" + (num1+num2))
+# For ascii code of any keystroke use ord()
+# print(ord('A'))
+# printing character from asii code
+# print(chr(97))
+# warlus operator 
+# print(name2:="whatever")
+# converting a text into different color
+# red color
+# print("\033[91mThis is red Color what are u sayingf \033[0m")
+# print("\033[94mwhat are u doing \033[0m")
+# opening a cartoon in browser
+# import antigravity
+# from datetime import date
+# print(date.today())
