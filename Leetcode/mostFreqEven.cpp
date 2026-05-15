eaed7c2b59a47d1d20c5a57f43af0ca24c26e5be
Mohit Tiwari <mt0988145@gmail.com>
2026-05-15 22:47:42 +0530

    adding files

---
+++ Leetcode/mostFreqEven.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    class Solution {
+public:
+    int mostFrequentEven(vector<int>& nums) {
+    int hash[100001]={0};
+        for(const int x:nums) {
+            if((x%2)==0 ) hash[x]++;
+        }
+        int fa = -1;
+        for(int i = 0;i<100001;i += 2){
+            if(hash[i]>fa && hash[i]!=0) fa = i;
+        }
+        return fa;
+    }
+};
+    return 0;
+}
